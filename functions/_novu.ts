eb774c2907963756bc4067e71177c7f11fc75912
JF-PS <91453739+doooooooooots@users.noreply.github.com>
2023-02-20 19:56:12 +0100

    add

--- functions/_novu.ts
+++ functions/_novu.ts
@@ -1,5 +1,6 @@
 import { Novu } from "@novu/node";
 
-const NOVU_API_SECRET = process.env.NOVU_API_SECRET;
+const NOVU_API_SECRET =
+  process.env.NOVU_API_SECRET || "86186edb890f78cba6f41d4ba05c8409";
 
 export const novu = new Novu(NOVU_API_SECRET);

1746551334c7d5667586fed9a1cfc09578579928
JF-PS <91453739+doooooooooots@users.noreply.github.com>
2023-02-20 18:03:05 +0100

    test

--- functions/nhost.ts
+++ functions/nhost.ts
@@ -1,6 +1,8 @@
 import { NhostClient } from "@nhost/nhost-js";
 
+const ADMIN_HASURA_SECRET = process.env.ADMIN_HASURA_SECRET || "";
+
 export const nhost = new NhostClient({
   subdomain: "localhost",
-  adminSecret: "nhost-admin-secret",
+  adminSecret: ADMIN_HASURA_SECRET,
 });
